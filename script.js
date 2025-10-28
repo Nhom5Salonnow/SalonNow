ef255459deb9a4dfbbe7046a4afcb1e7c88eac01
KHOADO\dotru <dotruongkhoa04@gmail.com>
2025-10-28 23:51:37 +0700

    add page

---
+++ script.js
@@ -0,0 +1,7 @@
+// Initialize Lucide icons
+lucide.createIcons();
+
+// Smooth scroll function
+function scrollToSection(id) {
+  document.getElementById(id)?.scrollIntoView({ behavior: "smooth" });
+}
